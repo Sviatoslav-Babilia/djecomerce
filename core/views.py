ee7108f689fdba0f076ad23a605cff194ea2ab6d
Sviatoslav Babilia <babilja@gmail.com>
2022-07-26 17:51:03 +0300

    add bootstrap html-blanks for basepage, checkout and products

--- core/views.py
+++ core/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
+from django.views import View
 from .models import Item
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView
 
 class ItemListView(ListView):
     model = Item
-    template_name = 'item_list.html'
+    template_name = 'home-page.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['items'] = Item.objects.all()
         return context
 
+
+class OrderItemView(ListView):
+    template_name = 'checkout-page.html'
+    model = Item
+
+
+class OrderItemView(ListView):
+    template_name = 'product-page.html'
+    model = Item
